99d473ba60ae47fc45fcf15d6846df80fda19b5a
George Schizas <gschizas@gmail.com>
2015-06-28 19:05:52 +0300

    Config

--- SqliteSession.py
+++ SqliteSession.py
@@ -71,7 +71,8 @@ class SqliteSession(MutableMapping, SessionMixin):
         return self.conn
 
     def _db_filename(self):
-        return '~/app-root/repo/session.db'
+        filename = '/tmp/session.db'
+        return 
 
     # These proxy classes are needed in order
     # for this session implementation to work properly. 
