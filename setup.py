f915dbc1b50b41f52c927ba8ec2a7d8c312a748d
George Schizas <gschizas@gmail.com>
2015-06-28 03:27:10 +0300

    Initial setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+setup(name='ModHelper',
+      version='0.1',
+      description='Mod Helper for Reddit',
+      author='George Schizas',
+      author_email='gschizas@gmail.com',
+      url='http://modhelper-terrasoftgreece.rhcloud.com/',
 #      install_requires=['Django>=1.3'],
      )
