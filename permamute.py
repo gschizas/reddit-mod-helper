bdf5393fe9613f2388e8fcbc7a49d257ad5e5939
George Schizas <gschizas@gmail.com>
2016-10-10 23:57:50 +0300

    Permamute now expects a YAML wiki page

--- permamute.py
+++ permamute.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from bot import RedditAgent
+import ruamel.yaml as yaml
+
 r = RedditAgent(ini_section='muteuser')
 sr = r.get_subreddit('europe')
 muted_users = list(sr.get_muted(user_only=False))
 muted_usernames = [user['name'].name for user in muted_users]
-permamutes = sr.get_wiki_page('permamuted').content_md.splitlines()
+permamutes = yaml.load(sr.get_wiki_page('permamuted').content_md)
 
 for permamute in permamutes:
     print(permamute, permamute in muted_usernames)
